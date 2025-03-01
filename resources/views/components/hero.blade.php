50788fe05a065c1c938ef76c23fbde80901d0b76
Matteo Trotta <matteotrotta7@gmail.com>
2025-03-01 11:40:20 +0100

    loogo improved

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,9 +1,9 @@
 <div class="container-fluid p-0 mx-0">
     <div class="row justify-content-center align-items-center">
-        <div class="col-12">
+        <div class="col-12 text-center">
 
             <section class="bg-black">
-                <video class="bg-black" autoplay muted loop src="{{ asset('/storage/video/mt.mp4') }}"></video>
+                <video class="bg-black" autoplay muted loop src="{{ asset('/storage/video/mt (1).mp4') }}"></video>
             </section>
 
             <video autoplay muted loop src="https://cdn.pixabay.com/video/2023/07/27/173481-849645868_large.mp4"
