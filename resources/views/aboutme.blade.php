be1dd9173c0914cb801aaecdf4f3c7d420012dfb
Matteo Trotta <matteotrotta7@gmail.com>
2025-03-06 17:38:11 +0100

    about-me

--- resources/views/aboutme.blade.php
+++ resources/views/aboutme.blade.php
@@ -3,47 +3,34 @@
 
 
     <div class="container my-5 ">
-        <div class="row text-center justify-content-center align-items-center">
-            <div class="col-10 justify-content-center align-items-center text-center">
+        <div class="row text-center justify-content-center ">
+            <div class="col-10 justify-content-center text-center">
 
-
+<hr class="my-5">
             </div>
         </div>
     </div>
 
-{{-- 
-    <div class="container-fluid my-5">
-        <div class="row text-center justify-content-center align-items-center my-5">
-            <div class="col-10 my-5">
-
-                <h1 class="tangerine-regular letter-spacing text-shadow my-5">
-
-                    Matteo Trotta <br> Freelance Web Developer
-
-                </h1>
-
-            </div>
-        </div>
-    </div> --}}
 
 
     <div class="container">
-        <div class="row justify-content-between align-items-center">
+        <div class="row border-5 md:mx-0 border-yellow-500 md:p-5 bg-white rounded justify-content-between align-items-center">
 
-            <div class="col-5  align-items-center justify-content-end px-0">
+            <div class="col-12 col-md-5 justify-content-center align-items-center ">
 
-<img class="rounded-circle shadow-2xl " src="{{ asset('/storage/images/fotoCv.jpg') }}" alt="foto di Matteo trotta">
+<img class="rounded-circle border-yellow-500 border-3 shadow-2xl mx-auto " src="{{ asset('/storage/images/fotoCv.jpg') }}" alt="foto di Matteo trotta">
 
             </div>
 
-            <div class="col-5 align-items-center justify-content-start">
 
-<h2 class=" tangerine-regular letter-spacing text-shadow my-5 ">Ciao , mi presento</h2>
 
-<p class="fs-4 border-3 p-5 bg-blue-800">Mi chiamo Matteo ,Da sempre appassionato di tecnologia , Cocktail bar , Tolkien e aspiro ancora, ad una vita nella Contea. ho trasformato la mia curiosità in una professione: sviluppo siti web che non solo funzionano bene, ma che siano anche piacevoli da usare.
+            <div class="col-12 col-md-6 align-items-center justify-content-center text-start border-1 border-yellow-500 rounded-circle">
 
-    Il mio percorso non è iniziato con il codice, ma dietro il bancone. Ho lavorato per anni come bartender in diversi contesti, dai ristoranti moderni agli hotel, fino ai cocktail bar più sperimentali. Ciò che mi ha sempre affascinato di questo mondo è la ricerca: la storia degli ingredienti, il loro utilizzo nelle diverse culture, lo studio delle materie prime e delle tecniche per creare un menu capace di raccontare un’esperienza. È la stessa attenzione al dettaglio e alla sperimentazione che oggi porto nello sviluppo web, creando interfacce su misura, curate e funzionali.
-    .</p>
+<h2 class=" tangerine-regular letter-spacing text-shadow md:my-5 ">hello World!</h2>
+
+<p class="fs-4  bg-white shadow-xl md:p-5 bodoni rounded-circle">Matteo Trotta ,da sempre appassionato di tecnologia , Cocktail bar e Tolkien e aspiro ancora, ad una vita nella Contea. <br> Ho trasformato la mia curiosità in una professione: Sviluppo siti web che non solo funzionano bene, ma che siano anche piacevoli da usare.
+
+    Il mio percorso non è iniziato con il codice, ma dietro il bancone. <br> Ho lavorato per anni come bartender in diversi contesti, dai ristoranti moderni agli hotel, fino ai cocktail bar più sperimentali. <br> Ciò che mi ha sempre affascinato di questo mondo è la ricerca: la storia degli ingredienti, il loro utilizzo nelle diverse culture, lo studio delle materie prime e delle tecniche per creare un menu capace di raccontare un’esperienza. <br> È la stessa attenzione al dettaglio e alla sperimentazione che oggi porto nello sviluppo web, creando interfacce su misura, curate e funzionali.</p>
 
             </div>
 
