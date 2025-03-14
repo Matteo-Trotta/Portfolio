f7061d75eea7e916ad3157717b7f127dc9b7d2bb
Matteo Trotta <matteotrotta7@gmail.com>
2025-03-14 10:30:29 +0100

    rotta ok

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 
-Route::get('/welcome', [PublicController::class ,'welcome'])->name('welcome');
+Route::get('/', [PublicController::class ,'welcome'])->name('welcome');
 
 Route::get('/aboutMe',[PublicController::class, 'aboutMe'])->name('aboutME');
 
