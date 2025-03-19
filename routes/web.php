bfb6f5500bc12f9131580a302b1bd0c567788133
Matteo Trotta <matteotrotta7@gmail.com>
2025-03-19 16:38:56 +0100

    aggiunto reindirizzamento per welcome

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 
-Route::get('/', [PublicController::class ,'welcome'])->name('welcome');
+Route::get('/', [PublicController::class, 'welcome'])->name('home');
+Route::get('/welcome', [PublicController::class, 'welcome'])->name('welcome');
+Route::redirect('/welcome', '/');
+
 
 Route::get('/aboutMe',[PublicController::class, 'aboutMe'])->name('aboutMe');
 
