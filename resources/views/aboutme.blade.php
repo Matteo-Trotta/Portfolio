1314f20ebfcac85ee5ea3c8efff6965b6457b35a
Matteo Trotta <matteotrotta7@gmail.com>
2025-04-02 15:49:36 +0200

    footer modificato

--- resources/views/aboutme.blade.php
+++ resources/views/aboutme.blade.php
@@ -6,7 +6,7 @@
         <div class="row text-center justify-content-center ">
             <div class="col-10 justify-content-center text-center">
 
-<hr class="my-5">
+                <hr class="my-5">
             </div>
         </div>
     </div>
@@ -14,11 +14,13 @@
 
 
     <div class="container mb-5">
-        <div class="row border-5 md:mx-0 border-yellow-500 md:p-5 bg-white rounded justify-content-between align-items-center">
+        <div
+            class="row border-5 md:mx-0 border-yellow-500 md:p-5 bg-white rounded justify-content-between align-items-center">
 
             <div class="col-12 col-md-5 justify-content-center align-items-center ">
 
-<img class="rounded-circle border-yellow-500 border-3 shadow-2xl mx-auto " src="https://media.istockphoto.com/id/165964685/it/vettoriale/night-cielo.jpg?s=612x612&w=0&k=20&c=hhZp-kl5zV9VyGO1CzOZMEWqNjovMPPVVWvnK-duKao=">
+                <img class="rounded-circle border-yellow-500 border-3 shadow-2xl mx-auto "
+                    src="{{ asset('storage/images/fotoCv.jpg') }}">
 
             </div>
 
@@ -26,19 +28,31 @@
 
             <div class="col-12 col-md-6 align-items-center justify-content-center text-start ">
 
-<h2 class=" tangerine-regular letter-spacing  md:my-5 ">hello World !</h2>
+                <h2 class=" tangerine-regular letter-spacing  md:my-5 ">hello World !</h2>
 
-<p class="fs-4  bg-white shadow-xl md:p-5 bodoni text-justify ">
+                <p class="fs-4  bg-white shadow-xl md:p-5 bodoni text-justify ">
 
-    Ciao! Mi chiamo Matteo,ho 31 anni e sono un appassionato di mondi fantasy, fotografia e programmazione web.  
-    
-    Fin da piccolo, le storie di Tolkien mi hanno affascinato: terre inesplorate, lingue misteriose e avventure epiche hanno nutrito la mia immaginazione. Questa passione per la narrazione visiva si riflette anche nella mia fotografia, dove cerco di catturare momenti che raccontano una storia, che siano paesaggi naturali o dettagli urbani.  
-    
-    Nel mio percorso professionale ho avuto esperienze in contesti dove precisione e cura del dettaglio erano fondamentali. Ho lavorato come bartender in ambienti raffinati, dove ogni drink era un equilibrio tra tecnica e creatività, e ogni esperienza offerta al cliente era studiata nei minimi particolari. Questo approccio meticoloso si riflette anche nel mio lavoro nel web: ogni sito che creo è progettato con attenzione, eleganza e funzionalità, per offrire un’esperienza fluida e piacevole.  
-    
-    Oltre a esplorare terre immaginarie e catturare immagini, mi piace costruire mondi digitali con il codice. Creo siti web moderni, funzionali e ben ottimizzati, unendo estetica e usabilità. La mia filosofia è offrire soluzioni professionali con un approccio chiaro e accessibile, perché la tecnologia dovrebbe essere un alleato, non un ostacolo.  
-    
-    Se vuoi scoprire di più su quello che faccio o collaborare su un progetto, contattami! 🚀
+                    Ciao! Mi chiamo Matteo,ho 31 anni e sono un appassionato di mondi fantasy, fotografia e
+                    programmazione web.
+
+                    Fin da piccolo, le storie di Tolkien mi hanno affascinato: terre inesplorate, lingue misteriose e
+                    avventure epiche hanno nutrito la mia immaginazione. Questa passione per la narrazione visiva si
+                    riflette anche nella mia fotografia, dove cerco di catturare momenti che raccontano una storia, che
+                    siano paesaggi naturali o dettagli urbani.
+
+                    Nel mio percorso professionale ho avuto esperienze in contesti dove precisione e cura del dettaglio
+                    erano fondamentali. Ho lavorato come bartender in ambienti raffinati, dove ogni drink era un
+                    equilibrio tra tecnica e creatività, e ogni esperienza offerta al cliente era studiata nei minimi
+                    particolari. Questo approccio meticoloso si riflette anche nel mio lavoro nel web: ogni sito che
+                    creo è progettato con attenzione, eleganza e funzionalità, per offrire un’esperienza fluida e
+                    piacevole.
+
+                    Oltre a esplorare terre immaginarie e catturare immagini, mi piace costruire mondi digitali con il
+                    codice. Creo siti web moderni, funzionali e ben ottimizzati, unendo estetica e usabilità. La mia
+                    filosofia è offrire soluzioni professionali con un approccio chiaro e accessibile, perché la
+                    tecnologia dovrebbe essere un alleato, non un ostacolo.
+
+                    Se vuoi scoprire di più su quello che faccio o collaborare su un progetto, contattami! 🚀
 
             </div>
 
