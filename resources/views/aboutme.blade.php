2dba1757f4b29c3d92571308c7908eb54c4ba28b
Matteo Trotta <matteotrotta7@gmail.com>
2025-04-07 17:23:01 +0200

    modificata pagina aboutMe

--- resources/views/aboutme.blade.php
+++ resources/views/aboutme.blade.php
@@ -13,28 +13,30 @@
 
 
 
-    <div class="container-fluid mb-5 px-0">
-        <div class="row mx-0  md:mx-0 md:p-5 bg-white rounded justify-content-between align-items-center">
+    <div class="container-fluid px-0 my-0">
+        <div class="row mx-0 md:mx-0 md:p-5 bg-white justify-content-between align-items-center">
 
 
 
 
-            <div class="col-6  align-items-center justify-content-center text-start ">
+            <div class="col-10 col-md-6  align-items-center justify-content-center text-start">
 
-                <h2 class=" tangerine-regular letter-spacing  md:my-5 ">hello World !</h2>
+                <h2 class=" tangerine display-1 letter-spacing !fs-2  md:my-5 ">hello World !</h2>
 
-                <p class="fs-5  bg-white md:p-5 bodoni text-justify justify-self-end w-50">
+
+                <p class="fs-5  bg-white md:p-5 bodoni text-justify justify-self-end w-50 ">
 
                     Ho iniziato con la passione per i mondi fantasy e il bartending, poi ho scoperto nella
-                    programmazione web il modo perfetto per costruire realtà digitali eleganti e funzionali. Ora unisco
+                    programmazione web il modo perfetto per costruire realtà digitali eleganti e funzionali. Ora
+                    unisco
                     creatività e codice per raccontare storie online.
-
+                </p>
             </div>
 
 
-            <div class="col-6 ">
+            <div class="col-10 col-md-6 ">
 
-                <img class="shadow-2xl img-fluid mx-auto rounded "
+                <img class="shadow-2xl img-fluid mx-auto rounded  "
                     src="{{ asset('storage/images/foto-cv-canva.jpg') }}">
 
             </div>
@@ -46,48 +48,44 @@
 
 
 
-    <div class="container-fluid mb-5 px-0">
-        <div class="row mx-0  md:mx-0 md:p-5 bg-white rounded justify-content-between align-items-center">
-
-
-            <div class="col-6 ">
-
-                <img class="shadow-2xl img-fluid mx-auto rounded "
+    <div class="container-fluid px-0">
+        <div class="row mx-0 md:mx-0 md:p-5 bg-white justify-content-between align-items-center">
+    
+            <!-- Colonna immagine -->
+            <div class="col-12 col-md-6 order-2 order-md-1">
+                <img class="shadow-2xl img-fluid mx-auto rounded"
                     src="{{ asset('storage/images/presto.it/Slide Pagina-Intera-Home.jpg') }}">
-
             </div>
-
-
-            <div class="col-6  align-items-center justify-content-center text-start ">
-
-                <h2 class=" tangerine-regular letter-spacing  md:my-5 ">Web Development</h2>
-
-                <p class="fs-5  bg-white md:p-5 bodoni text-justify justify-self-start ms-5 w-50">
-
+    
+            <!-- Colonna testo -->
+            <div class="col-12 col-md-6 align-items-center justify-content-center text-start order-1 order-md-2">
+                <h2 class="tangerine display-1 letter-spacing md:my-5">Web Development</h2>
+    
+                <p class="fs-5 bg-white md:p-5 bodoni text-justify justify-self-start ms-5 w-50">
                     Dopo anni dietro il bancone, ho deciso di esplorare un nuovo linguaggio: quello del web. Ho iniziato
-                    con un corso intensivo online, scoprendo che anche con il codice si possono creare esperienze.</p>
+                    con un corso intensivo online, scoprendo che anche con il codice si possono creare esperienze.
+                </p>
                 <br>
-                <p class="fs-5  bg-white md:p-5 bodoni text-justify justify-self-center me-5 w-75"> Ho lavorato in team
-                    utilizzando Git per il versionamento del codice e la collaborazione sullo sviluppo. con diversi
-                    linguaggi come HTML, CSS e JavaScript, PHP , framework Laravel e MySql per
-                    realizzare un e-commerce di compra/vendita articoli, con integrazione di IA. Questo mi ha permesso
-                    di
-                    creare siti web che offrono un'esperienza utente intuitiva e coinvolgente.
+                <p class="fs-5 bg-white md:p-5 bodoni text-justify justify-self-center me-5 w-75">
+                    Ho lavorato in team utilizzando Git per il versionamento del codice e la collaborazione sullo sviluppo,
+                    con diversi linguaggi come HTML, CSS e JavaScript, PHP, framework Laravel e MySql per realizzare un
+                    e-commerce di compra/vendita articoli, con integrazione di IA. Questo mi ha permesso di creare siti web
+                    che offrono un'esperienza utente intuitiva e coinvolgente.
                 </p>
             </div>
-
-
+    
         </div>
     </div>
+    
 
 
     <div class="container-fluid mb-5 px-0">
-        <div class="row mx-0  md:mx-0 md:p-5 bg-white rounded justify-content-between align-items-center">
+        <div class="row mx-0  md:mx-0 md:p-5 bg-white justify-content-between align-items-center">
 
 
-            <div class="col-6  align-items-center justify-content-center text-start ">
+            <div class="col-12 col-md-6  align-items-center justify-content-center text-start ">
 
-                <h2 class=" tangerine-regular letter-spacing  md:my-5 ">Fotografia</h2>
+                <h2 class=" tangerine display-1 letter-spacing  md:my-5 ">Fotografia</h2>
 
                 <p class="fs-5  bg-white md:p-5 bodoni text-justify justify-self-start ms-5 w-50">
 
@@ -101,7 +99,7 @@
             </div>
 
 
-            <div class="col-6 ">
+            <div class="col-12 col-md-6 ">
 
                 <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-indicators">
@@ -111,27 +109,31 @@
                             aria-label="Slide 2"></button>
                         <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="2"
                             aria-label="Slide 3"></button>
-                            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="3"
+                        <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="3"
                             aria-label="Slide 4"></button>
                     </div>
 
                     <div class="carousel-inner vh-50">
-{{-- ITEM 1--}}
+                        {{-- ITEM 1 --}}
                         <div class="carousel-item active">
-                            <img class="" src="storage/images/aboutMe/verticali/scimmia-scuro.JPG" class="d-block w-100" alt="Slide 1">
+                            <img class="" src="storage/images/aboutMe/verticali/scimmia-scuro.JPG"
+                                class="d-block w-100" alt="Slide 1">
                         </div>
-{{-- ITEM 2 --}}
+                        {{-- ITEM 2 --}}
                         <div class="carousel-item">
-                            <img src="storage/images/aboutMe/verticali/dettaglio-bottigliera.JPG" class="d-block w-100" alt="Slide 2">
+                            <img src="storage/images/aboutMe/verticali/dettaglio-bottigliera.JPG" class="d-block w-100"
+                                alt="Slide 2">
                         </div>
-{{-- ITEM 3 --}}
+                        {{-- ITEM 3 --}}
                         <div class="carousel-item">
-                            <img src="storage/images/aboutMe/verticali/bulldog.JPG" class="d-block w-100" alt="Slide 3">
+                            <img src="storage/images/aboutMe/verticali/bulldog.JPG" class="d-block w-100"
+                                alt="Slide 3">
                         </div>
 
-{{-- ITEM 4 --}}
+                        {{-- ITEM 4 --}}
                         <div class="carousel-item">
-                            <img src="storage/images/aboutMe/verticali/montelobos.JPG" class="d-block w-100" alt="Slide 3">
+                            <img src="storage/images/aboutMe/verticali/montelobos.JPG" class="d-block w-100"
+                                alt="Slide 3">
                         </div>
 
                     </div>
