6d48582de02ac586a56c26669b3eec9fe29d4a6a
Matteo Trotta <matteotrotta7@gmail.com>
2025-04-11 15:22:26 +0200

    aggiunta favicon e contatti

---
+++ resources/views/components/contatti.blade.php
@@ -0,0 +1,23 @@
+<div class="container-fluid bg-white">
+    <div class="row justify-content-center align-items-center">
+        <div class="col-10 text-center">
+
+            <h3 class="tangerine-regular">
+                Metodi di contatto
+            </h3>
+
+
+            <ul>
+                <li class="bodoni fs-3 italic"><i class="fa-solid fa-envelope" style="color: #d72323;"></i>
+                    matteotrotta7@gmail.com</li>
+                <li class="bodoni fs-3 italic"><i class="fa-brands fa-whatsapp" style="color: #39e12d;"></i> (+39) 339 2222
+                    022</li>
+                <li class="bodoni fs-3"> Linked<i class="fa-brands fa-linkedin"></i> : <a class="text-blue-400 hover:underline"
+                        href="https://www.linkedin.com/in/matteo-trotta-5a9984278/"> Visita profilo</a></li>
+            </ul>
+
+
+
+        </div>
+    </div>
+</div>
