2c0ff82137a2693983196520cdeaa710ac4df8ee
Matteo Trotta <matteotrotta7@gmail.com>
2025-05-02 12:11:56 +0200

    sostituito video.mp4 con video .webm

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -9,7 +9,7 @@
 
 
             {{-- video mostrato solamente su dispositivi lg --}}
-            <video aria-hidden="true" alt="video sfondo sezione" autoplay loop muted playsinline src="{{ asset('storage/video/video-hero.mp4') }}" id="heroVideo"
+            <video aria-hidden="true" alt="video sfondo sezione" autoplay loop muted playsinline src="{{ asset('storage/video/video-hero.webm') }}" id="heroVideo"
                 class=" d-none d-lg-block position-absolute">
             </video>
 
