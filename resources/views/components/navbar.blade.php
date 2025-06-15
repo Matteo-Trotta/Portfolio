633581d32dd7882a550406708ebdffc27ba0152f
Matteo Trotta <matteotrotta7@gmail.com>
2025-06-15 11:45:59 +0200

    toto boostrap

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -30,13 +30,14 @@
                 </li>
 
                 <div class="dropdown">
-                    <button class="dropbtn ">Progetti</button>
+                    <button class="dropbtn btn btn-success p-3 fs-4">Progetti</button>
                     <div class="dropdown-content">
                         <a href="https://blog.sitiwebromatrottamatteo.com/">Blog Marketing</a>
                         <a href="https://wordpress.sitiwebromatrottamatteo.com/">Cantina Sala</a>
                         <a href="#">in arrivo . .</a>
                     </div>
                 </div>
+
 
 
 
@@ -82,13 +83,8 @@
                 </li>
 
                 <li class="hover:translate-y-2">
-                    <a href="{{ 'preventivo' }}" class="fw-semibold fs-3 italic text-lg ">Preventivi
-                    </a>
-                </li>
-
-                <li class="hover:translate-y-2">
-                    <a href="{{ 'https://blog.sitiwebromatrottamatteo.com/' }}"
-                        class="fw-semibold fs-3 italic text-lg ">Blog
+                    <a href="{{ 'preventivo' }}"
+                        class="fw-semibold fs-3 italic text-lg ">Preventivi
                     </a>
                 </li>
 
