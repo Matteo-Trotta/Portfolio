11284dfe3c0f87d9cd833add745a33e507eeba88
Matteo Trotta <matteotrotta7@gmail.com>
2025-07-04 16:40:53 +0200

    aggiunto meta tag og:site_name

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,10 +18,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description"
-        content="Hai bisogno di un sito web professionale? Creo soluzioni digitali su misura per aziende e liberi professionisti,a Roma.">
 
-    <title>Web Developer Freelance Roma | Siti Web | Landing Page | QR Code Menù personalizzati </title>
+    {{-- search result --}}
+    <meta property="og:site_name" content="Matteo Trotta - Web Developer">
+    <title>Web Developer Freelance Roma | Trotta Matteo</title>
+    <meta name="description"
+        content="Hai bisogno di un sito web professionale? Creo soluzioni digitali su misura per aziende e liberi professionisti.">
+ 
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -33,8 +36,8 @@
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 
     {{-- COOKIE IUBENDA --}}
-    <script type="text/javascript" src="//embeds.iubenda.com/widgets/06b020d7-268c-4851-afdc-61a5fbeab44a.js"></script>
-    <script type="text/javascript" src="//embeds.iubenda.com/widgets/06b020d7-268c-4851-afdc-61a5fbeab44a.js"></script>
+    {{-- <script type="text/javascript" src="//embeds.iubenda.com/widgets/06b020d7-268c-4851-afdc-61a5fbeab44a.js"></script> --}}
+    {{-- <script type="text/javascript" src="//embeds.iubenda.com/widgets/06b020d7-268c-4851-afdc-61a5fbeab44a.js"></script> --}}
     {{-- COOKIE IUBENDA --}}
     <link
         href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAADAwMA//8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAEREAAAARAAAAAAAAAAEQAAAAAAAAABEAAAAAAAAAARAAAAAAAAABEAAAAAAAABEAAAAAAAABEAAAAAAAABEAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
