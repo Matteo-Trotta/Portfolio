2a6220d2e3431d2b7873e3928670a58ae0f4db95
Matteo Trotta <matteotrotta7@gmail.com>
2025-07-09 16:29:58 +0200

    cambiato il formato di tutte le immagini cards,ora webp,nella cartella cards-webp con ImageMagick,ridimensionate per ogni tipologia di visualizzazione

--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -4,11 +4,12 @@
 
         <div class="card primafilacard shadow-lg border-blue-500 " style="height: 600px">
             <div class="img-container">
-
-                <img class="img-fluid rounded border-4"
-                    src="{{ asset('storage/images/cards/sito-web.webp') }}" class="card-img-top"
-                    alt="...">
-
+                <picture>
+                    <source srcset="{{ asset('storage/images/cards-webp/sito-web.webp') }}" media="(max-width: 767px)" type="image/webp">
+                    <source srcset="{{ asset('storage/images/cards-webp/sito-web.webp') }}" media="(max-width: 1024px)" type="image/webp">
+                    <source srcset="{{ asset('storage/images/cards-webp/sito-web.webp') }}" media="(min-width: 1025px)" type="image/webp">
+                    <img class="img-fluid rounded border-4" src="{{ asset('storage/images/cards/sito-web.webp') }}" alt="Sito Web">
+                </picture>
             </div>
             <div class="card-body overflow-scroll text-center align-items-center">
                 <h5 class="card-title fs-1 bodoni">Sito Web</h5>
@@ -16,68 +17,59 @@
                 <p class="card-text playfair-display fs-4">Realizzo siti web veloci, sicuri e su misura per la tua
                     attività. Ogni progetto è unico e pensato per offrire un’esperienza utente perfetta su qualsiasi
                     dispositivo.</p>
-
                 <a href="{{ route('services') }}"
                     class="btn btn-warning shadow-2xl p-3 my-3  hover:scale-110 hover:bg-indigo-500  hover:text-white">Scopri
                     di più</a>
             </div>
-
         </div>
 
     </div>
-
 
     <div class="col-10 col-lg-3 my-5 justify-content-center align-items-center h-full ">
 
         <div class="card primafilacard shadow-lg border-blue-500 p-1 " style="height: 600px">
-            <img class="img-fluid" src="{{ asset('storage/images/cards/seo.png') }}"
-                class="card-img-top" alt="...">
-
+            <picture>
+                <source srcset="{{ asset('storage/images/cards-webp/seo-640.webp') }}" media="(max-width: 767px)" type="image/webp">
+                <source srcset="{{ asset('storage/images/cards-webp/seo-960.webp') }}" media="(max-width: 1024px)" type="image/webp">
+                <source srcset="{{ asset('storage/images/cards-webp/seo-1280.webp') }}" media="(min-width: 1025px)" type="image/webp">
+                <img class="img-fluid" src="{{ asset('storage/images/cards/seo.png') }}" alt="Consulenza SEO">
+            </picture>
             <div class="card-body overflow-scroll text-center align-items-center">
-
                 <h5 class="card-title fs-1 bodoni mt-5 pt-1">Consulenza SEO</h5>
                 <hr>
-
                 <p class="card-text playfair-display fs-4">Ottimizzo il tuo sito per migliorare la visibilità su
                     Google. Analizzo contenuti, parole chiave e prestazioni per aiutarti a raggiungere più clienti
                     online.
                 </p>
-
                 <a href="{{ route('services') }}"
                     class="btn btn-warning shadow-2xl p-3 my-3  hover:scale-120 hover:bg-indigo-500 hover:text-white ">Scopri
                     di più</a>
             </div>
-
         </div>
 
-
     </div>
-
 
     <div class="col-10 col-lg-3 my-5 justify-content-center align-items-center  ">
 
         <div class="card h-full primafilacard shadow-lg border-blue-500 p-1" style="height: 600px">
-
-            <img class="img-fluid rounded border-4"
-                src="{{ asset('storage/images/cards/landing-page.avif') }}"
-                class="card-img-top" alt="...">
-
+            <picture>
+                <source srcset="{{ asset('storage/images/cards-webp/landing-page-640.webp') }}" media="(max-width: 767px)" type="image/webp">
+                <source srcset="{{ asset('storage/images/cards-webp/landing-page-960.webp') }}" media="(max-width: 1024px)" type="image/webp">
+                <source srcset="{{ asset('storage/images/cards-webp/landing-page-1280.webp') }}" media="(min-width: 1025px)" type="image/webp">
+                <img class="img-fluid rounded border-4" src="{{ asset('storage/images/cards/landing-page.avif') }}" alt="Landing Page">
+            </picture>
             <div class="card-body overflow-scroll text-center align-items-center">
-
                 <h5 class="card-title fs-1 bodoni">Landing Page</h5>
                 <hr>
                 <p class="card-text playfair-display fs-4">
                     Creo landing page mirate e persuasive, progettate per
                     convertire i visitatori in clienti. Perfette per promuovere prodotti, servizi o eventi speciali.
                 </p>
-
                 <a href="{{ route('services') }}"
                     class="btn btn-warning shadow-2xl p-3 my-3  hover:scale-110 hover:bg-indigo-500  hover:text-white">Scopri
                     di più</a>
             </div>
         </div>
-
-
 
     </div>
 </div>
@@ -85,104 +77,72 @@
 {{-- SECONDA FILA --}}
 <div class="row text-center justify-content-center align-items-center mx-0 lg:my-3">
 
-
     <div class="col-10 col-lg-3 my-5 secondafilacard justify-content-center align-items-center  text-center">
 
         <div class="card h-full shadow-lg border-blue-500" style="height: 600px">
-
-            <img class="img-fluid"
-                src="{{ asset('storage/images/cards/responsive-design.avif') }}"
-                class="card-img-top" alt="...">
-
+            <picture>
+                <source srcset="{{ asset('storage/images/cards-webp/responsive-design-640.webp') }}" media="(max-width: 767px)" type="image/webp">
+                <source srcset="{{ asset('storage/images/cards-webp/responsive-design-960.webp') }}" media="(max-width: 1024px)" type="image/webp">
+                <source srcset="{{ asset('storage/images/cards-webp/responsive-design-1280.webp') }}" media="(min-width: 1025px)" type="image/webp">
+                <img class="img-fluid" src="{{ asset('storage/images/cards/responsive-design.avif') }}" alt="Responsive Web">
+            </picture>
             <div class="card-body overflow-scroll text-center align-items-center ">
-
-
-
                 <h5 class="card-title fs-1 bodoni">Responsive Web Mobile-First</h5>
-
                 <hr>
-
                 <p class="card-text playfair-display fs-4"> siti web ottimizzati per smartphone,
                     garantendo un’esperienza fluida e intuitiva su tutti i dispositivi, dal mobile al desktop.
                 </p>
-
                 <a href="{{ route('services') }}"
                     class="btn btn-warning shadow-2xl p-3 my-3  hover:scale-110 hover:bg-indigo-500  hover:text-white">Scopri
                     di più
-
                 </a>
-
             </div>
-
         </div>
 
-
     </div>
-
-
 
     <div class="col-10 col-lg-3 my-5 secondafilacard justify-content-center align-items-center ">
 
         <div class="card h-full shadow-lg border-blue-500 p-1 "style="height: 600px">
-
-            <img class="img-fluid"
-                src="{{ asset('storage/images/cards/seo-trend.avif') }}"
-                alt="...">
-
+            <picture>
+                <source srcset="{{ asset('storage/images/cards-webp/seo-trend-640.webp') }}" media="(max-width: 767px)" type="image/webp">
+                <source srcset="{{ asset('storage/images/cards-webp/seo-trend-960.webp') }}" media="(max-width: 1024px)" type="image/webp">
+                <source srcset="{{ asset('storage/images/cards-webp/seo-trend-1280.webp') }}" media="(min-width: 1025px)" type="image/webp">
+                <img class="img-fluid" src="{{ asset('storage/images/cards/seo-trend.avif') }}" alt="Google trends for SEO">
+            </picture>
             <div class="card-body overflow-scroll text-center align-items-center">
-
-                <h5 class="card-title fs-1 bodoni">Google trends for SEO's
-
-                </h5>
-
+                <h5 class="card-title fs-1 bodoni">Google trends for SEO's</h5>
                 <hr>
-
                 <p class="card-text playfair-display fs-4">
                     Analizzo il tuo sito e i trend di ricerca con Google
                     Trends per ottimizzare contenuti e chiave.
                 </p>
-
                 <a href="{{ route('services') }}"
                     class="btn btn-warning shadow-2xl p-3 my-3  hover:scale-110 hover:bg-indigo-500  hover:text-white">Scopri
                     di più</a>
             </div>
         </div>
 
-
-
-
-
     </div>
-
-
 
     <div class="col-10 col-lg-3 my-5 secondafilacard justify-content-center align-items-center ">
         <div class="card h-full shadow-lg border-blue-500 p-1 " style="height: 600px">
-            <img class="img-fluid"
-                src="{{ asset('storage/images/cards/hosting.avif') }}"
-                class="card-img-top" alt="...">
-
+            <picture>
+                <source srcset="{{ asset('storage/images/cards-webp/hosting-640.webp') }}" media="(max-width: 767px)" type="image/webp">
+                <source srcset="{{ asset('storage/images/cards-webp/hosting-960.webp') }}" media="(max-width: 1024px)" type="image/webp">
+                <source srcset="{{ asset('storage/images/cards-webp/hosting-1280.webp') }}" media="(min-width: 1025px)" type="image/webp">
+                <img class="img-fluid" src="{{ asset('storage/images/cards/hosting.avif') }}" alt="Hosting">
+            </picture>
             <div class="card-body overflow-scroll text-center align-items-center ">
-
                 <h5 class="card-title fs-1 bodoni">Hosting</h5>
-
                 <hr>
-
                 <p class="card-text playfair-display fs-4 ">Offro soluzioni di hosting affidabili e manutenzione
                     continua. Il tuo sito sarà sempre online, aggiornato e sicuro, senza che tu debba preoccuparti
                     di nulla.</p>
-
                 <a href="{{ route('services') }}"
                     class="btn btn-warning shadow-2xl p-3 my-3  hover:scale-110 hover:bg-indigo-500  hover:text-white">Scopri
                     di più</a>
             </div>
         </div>
-
-
-
-
-
     </div>
-
-</div>
 </div>
