14cd6563ef1ee6809a769ea648eda34654a62696
Matteo Trotta <matteotrotta7@gmail.com>
2025-08-30 11:29:44 +0200

    aggiunto attributo title al tag iframe del contact form di whatsapp per migliorare punteggio accessibilità

--- resources/views/components/contact-form-whatsapp.blade.php
+++ resources/views/components/contact-form-whatsapp.blade.php
@@ -3,7 +3,7 @@
     <div class="row ">
         <div class="col-12 ">
 
-            <iframe src="https://whatsform.com/oj1k4q" width="100%" height="1000" class="p-5 border-2 shadow-lg bg-gray-900"
+            <iframe title="Whatsapp Form per prenotazione appuntamento gratuito" src="https://whatsform.com/oj1k4q" width="100%" height="1000" class="p-5 border-2 shadow-lg bg-gray-900"
                 frameBorder="0"></iframe>
 
 
